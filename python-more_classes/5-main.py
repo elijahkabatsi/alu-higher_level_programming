fa94b4d3cc05e1e7693ce48d6431dc93481b72a1
elijahkabatsi <e.kabatsi@alustudent.com>
2025-07-22 23:38:48 +1400

    Internally

--- python-more_classes/5-main.py
+++ python-more_classes/5-main.py
@@ -2,10 +2,9 @@
 Rectangle = __import__('5-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(
-    my_rectangle.area(), 
-    my_rectangle.perimeter()
-))
+area = my_rectangle.area()
+perimeter = my_rectangle.perimeter()
+print("Area: {} - Perimeter: {}".format(area, perimeter))
 
 del my_rectangle
 
