2269e8f6ba29d53cd305103f7ab2a70de3b567d0
elijahkabatsi <e.kabatsi@alustudent.com>
2025-07-23 03:49:03 +1400

    IGITAMBO

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class with customizable print symbol and instance count."""
+"""Defines a Rectangle class with print symbol and instance count support."""
 
 
 class Rectangle:
