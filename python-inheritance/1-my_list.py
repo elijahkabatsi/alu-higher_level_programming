330985d27fe9a8337e73ce6dc725fb6bd65298c3
elijahkabatsi <e.kabatsi@alustudent.com>
2025-07-25 23:01:22 +1400

    Yeah

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,4 @@ class MyList(list):
     """Inherits from list and adds print_sorted method."""
 
     def print_sorted(self):
-        """Print the list elements in ascending sorted order."""
         print(sorted(self))
