926db3db5e2a93406cdd2fdb5b20c5d49bdb43d8
elijahkabatsi <e.kabatsi@alustudent.com>
2025-07-25 22:41:48 +1400

    Yeah

---
+++ python-inheritance/tests/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Custom list class with sorted print."""
+
+
+class MyList(list):
+    """Inherits from list and adds print_sorted method."""
+    def print_sorted(self):
+        print(sorted(self))
