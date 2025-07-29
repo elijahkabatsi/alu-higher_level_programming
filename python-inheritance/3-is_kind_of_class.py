72ee5e863be5fc218b68d4d884cc7e001724fbb9
elijahkabatsi <e.kabatsi@alustudent.com>
2025-07-30 01:44:13 +1400

    FOG

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that checks object-class relationships."""
 
+
 def is_kind_of_class(obj, a_class):
     """Return True if obj is an instance or subclass instance of a_class."""
     return isinstance(obj, a_class)
